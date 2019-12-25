6187e17410b8e6eed8e072e0b2de4b79d668dadc
gtproger <gtpr0ger@tutanota.com>
2019-12-25 20:12:01 +0300

    add functional which the convert morse code to cyrillic

--- morse.rb
+++ morse.rb
@@ -1,33 +1,39 @@
 class Morse
   def initialize
-    @dictionary = {
-        'a' => '.-',
-        'b' => '-...',
-        'c' => '-.-.',
-        'd' => '-..',
-        'e' => '.',
-        'f' => '..-.',
-        'g' => '--.',
-        'h' => '....',
-        'i' => '..',
-        'j' => '.---',
-        'k' => '-.-',
-        'l' => '.-..',
-        'm' => '--',
-        'n' => '-.',
-        'o' => '---',
-        'p' => '.--.',
-        'q' => '--.-',
-        'r' => '.-.',
-        's' => '...',
-        't' => '-',
-        'u' => '..-',
-        'v' => '...-',
-        'w' => '.--',
-        'x' => '-..-',
-        'y' => '-.--',
-        'z' => '--..',
-        ' ' => '',
+    @dict = {
+        'а' => '.-',
+        'б' => '-···',
+        'в' => '.--',
+        'г' => '--.',
+        'д' => '-..',
+        'е' => '.',
+        'ж' => '...-',
+        'з' => '--..',
+        'и' => '..',
+        'й' => '.---',
+        'к' => '-.-',
+        'л' => '.-..',
+        'м' => '--',
+        'н' => '-.',
+        'о' => '---',
+        'п' => '.--.',
+        'р' => '.-.',
+        'с' => '...',
+        'т' => '-',
+        'у' => '..-',
+        'ф' => '..-.',
+        'х' => '....',
+        'ц' => '-.-.',
+        'ч' => '---.',
+        'ш' => '----',
+        'щ' => '--.-',
+        'ъ' => '--.--',
+        'ы' => '-.--',
+        'ь' => '-.--',
+        'э' => '..-..',
+        'ю' => '..--',
+        'я' => '.-.-',
+        ' ' => '  ',
         '1' => '.----',
         '2' => '..---',
         '3' => '...--',
@@ -39,18 +45,21 @@ class Morse
         '9' => '---.',
         '0' => '-----'
     }
+    @encode_dict = @dict.invert
     @text = nil
     @result = nil
   end
 
   def inputStr
     puts "Введите текст для конвертации: "
+    print ">> "
     str = STDIN.gets.chomp.downcase
     @text = str
-    decode(@text)
+    #decode(@text)
+    #encode(@text)
   end
 
-  def decode(text)
+  def encode(text)
     # разбиваем строку на буквы, включая пробелы
     letters = text.split('')
 
@@ -58,13 +67,39 @@ class Morse
 
     # конвертация букв
     letters.each do |item|
-      if @dictionary.has_key?(item)
-        x = @dictionary[item] + " "
+      if @dict.has_key?(item)
+        x = @dict[item] + " "
         convert_letters << x
       end
     end
 
     # результат конвертирования
+    puts "Результат:"
+    @result = convert_letters.join('')
+  end
+
+  def decode(text)
+    words = text.split("    ")
+
+    letters = []
+
+    words.each do |item|
+      letters << item.split(" ")
+    end
+
+    convert_letters = []
+
+    letters.each do |item|
+      item.each do |item|
+        if @encode_dict.has_key?(item)
+          convert_letters << @encode_dict[item]
+        end
+      end
+      convert_letters << " "
+    end
+
+    convert_letters.pop
+
     puts "Результат:"
     @result = convert_letters.join('')
   end
