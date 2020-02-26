bf1374a3a86574555072b193cdf835f7a73f6a56
dionixs <dionixs@tutanota.com>
2020-02-26 13:11:03 +0300

    Update telegraph.rb

--- lib/telegraph.rb
+++ lib/telegraph.rb
@@ -9,12 +9,48 @@ module Telegraph
     # конвертация, на случай того если слово/текст содержит кириллицу
     text = Translit.convert(text, :english)
 
-    # разбиваем строку на буквы, включая пробелы
-    letters = text.split('')
+    # массив букв включая пробелы
+    letters = to_letters_array(text)
 
+    # результат конвертирования
+    convert_letters(letters).join('').strip
+  end
+
+  # Morse to Text
+  #
+  # Для упрощения в режиме morse_to_text:
+  # Между одним символом азбуки морзе, используется один пробел.
+  # Между словами отступ равен 4 пробелам.
+  # К примеру: "... --- ...    ... --- ...".
+  def self.morse_to_text(morse)
+    # массив слов
+    words = to_words_array(morse)
+
+    # массив, содержащий вложенные массивы
+    # в которых слова разбиты на символы азбуки морзе
+    letters = words_to_morse_char(words)
+
+    # массив расшифрованных букв
+    convert_letters = decode_chars(letters)
+
+    # удаляем последний элемент массива
+    convert_letters.pop
+
+    # результат конвертирования
+    convert_letters.join('')
+  end
+
+  private
+
+  # метод который разбивает строку на буквы, включая пробелы
+  def self.to_letters_array(string)
+    string.split('')
+  end
+
+  # метод который конвертирует массив букв, в символы азбуки Морзе
+  def self.convert_letters(letters)
     convert_letters = []
 
-    # конвертация букв
     letters.each do |item|
       if ENCODE_DICT.key?(item)
         x = ENCODE_DICT[item] + ' '
@@ -22,29 +58,30 @@ module Telegraph
       end
     end
 
-    # результат конвертирования
-    convert_letters.join('').strip
+    convert_letters
   end
 
-  # Morse to Text
-  #
-  # Для упрощения в режиме decode:
-  # Между одним символом азбуки морзе, используется один пробел.
-  # Между словами отступ равен 4 пробелам.
-  # К примеру: "... --- ...    ... --- ...".
-  def self.morse_to_text(morse)
-    # разбиваем строку на массив слов
-    words = morse.split('    ')
+  # метод который разбивает строку на массив слов
+  def self.to_words_array(morse)
+    morse.split('    ')
+  end
 
+  # метод который разбивает слова, на символы азбуки морзе
+  def self.words_to_morse_char(words)
     # массив, содержащий вложенные массивы
     # в которых слова разбиты на символы азбуки морзе
-    letters = []
+    chars = []
 
     # разбиваем слова на символы азбуки морзе
     words.each do |item|
-      letters << item.split(' ')
+      chars << item.split(' ')
     end
 
+    chars
+  end
+
+  # метод который возвращает массив расшифрованных букв
+  def self.decode_chars(letters)
     # массив для хранения расшифрованных букв
     convert_letters = []
 
@@ -57,9 +94,6 @@ module Telegraph
       convert_letters << ' '
     end
 
-    convert_letters.pop
-
-    # результат конвертирования
-    convert_letters.join('')
+    convert_letters
   end
 end
